6d3cbc2c9edcc24f23775aaf35514c2b5772332c
frmurgia <32902502+frmurgia@users.noreply.github.com>
2018-07-24 17:24:34 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,19 +5,19 @@ Inspired by:
 	Makio135's sketch www.openprocessing.org/sketch/385808
  */
 
-var freq=[261.63,293.66,329.63,392.00,440.00,493.88]
+var freq = [261.63, 293.66, 329.63, 392.00, 440.00, 493.88]
 
 var filter, filterFreq, filterRes, reverb;;
 
- var attackLevel = 1.0;
- var releaseLevel = 0;
+var attackLevel = 0.6;
+var releaseLevel = 0;
 
- var attackTime = 0.1
- var decayTime = 0.9 ;
- var susPercent = 0.6;
- var releaseTime = 1.1;
+var attackTime = 0.1
+var decayTime = 0.9;
+var susPercent = 0.6;
+var releaseTime = 1.1;
 
- var env, triOsc;
+var env, triOsc;
 
 var allParticles = [];
 var maxLevel = 5;
@@ -47,7 +47,7 @@ function Particle(x, y, level) {
     if (this.life % 10 == 0) {
       if (this.level > 0) {
         this.level -= 1;
-        var newParticle = new Particle(this.pos.x, this.pos.y, this.level-1);
+        var newParticle = new Particle(this.pos.x, this.pos.y, this.level - 1);
         allParticles.push(newParticle);
       }
     }
@@ -68,34 +68,34 @@ function setup() {
   // triOsc.disconnect();
 
   createCanvas(windowWidth, windowHeight);
-// triOsc.disconnect();
- reverb.process(triOsc, 4, 3);
-     reverb.connect(filter);
+  // triOsc.disconnect();
+  reverb.process(triOsc, 4, 3);
+  reverb.connect(filter);
   colorMode(HSB, 360);
 
   textAlign(CENTER);
 
-    background(255);
+  background(255);
 }
 
 
 function draw() {
 
   // frequency (150Hz) to the highest (15050) that humans can hear
-filterFreq = map (mouseX, 0, width, 10, 1050);
+  filterFreq = map(mouseX, 0, windowWidth, 150, 10050);
+  // console.log(filterFreq)
+  // Map mouseY to resonance (volume boost) at the cutoff frequency
+  filterRes = (10);
 
-// Map mouseY to resonance (volume boost) at the cutoff frequency
-filterRes = map(mouseY, 0, height, 10, 5);
-
-// set filter parameters
-filter.set(filterFreq, filterRes);
+  // set filter parameters
+  filter.set(filterFreq, filterRes);
   // Create fade effect.
   noStroke();
-   fill(360, 30);
-   rect(0, 0, width, height);
+  fill(360, 30);
+  rect(0, 0, width, height);
   // Move and spawn particles.
   // Remove any that is below the velocity threshold.
-  for (var i = allParticles.length-1; i > -1; i--) {
+  for (var i = allParticles.length - 1; i > -1; i--) {
     allParticles[i].move();
 
     if (allParticles[i].vel.mag() < 0.7) {
@@ -115,8 +115,8 @@ filter.set(filterFreq, filterRes);
     for (var i = 0; i < data.length; i += 3) {
       // Collect particles that make this triangle.
       var p1 = allParticles[data[i]];
-      var p2 = allParticles[data[i+1]];
-      var p3 = allParticles[data[i+2]];
+      var p2 = allParticles[data[i + 1]];
+      var p3 = allParticles[data[i + 2]];
 
       // Don't draw triangle if its area is too big.
       var distThresh = 75;
@@ -135,104 +135,99 @@ filter.set(filterFreq, filterRes);
 
       // Base its hue by the particle's life.
 
-        noStroke();
-        fill(165+p1.life*1.5, 360, 360);
+      noStroke();
+      fill(165 + p1.life * 1.5, 360, 360);
 
 
       triangle(p1.pos.x, p1.pos.y,
-               p2.pos.x, p2.pos.y,
-               p3.pos.x, p3.pos.y);
+        p2.pos.x, p2.pos.y,
+        p3.pos.x, p3.pos.y);
     }
   }
 
   noStroke();
-  fill(255,0,0);
+  fill(255, 0, 0);
 
 
 
 }
-// function mousePressed(){
-//   if ((mouseY >0 )&& mouseY < (windowHeight/3) ){
-//    triOsc.freq(220);
-//   }
-//   if (mouseY > (windowHeight/3)&& mouseY < (windowHeight) ){
-//    triOsc.freq(420);
-//   }
-//
-//
-//
-//
-// }
-
-
-
-
-
-
-
-
 
 function mouseReleased() {
   env.triggerRelease();
- }
+}
 
- function envAttack(){
+function envAttack() {
 
-   env.triggerAttack();
+  env.triggerAttack();
 }
 
 
-function griglia() {
-  stroke(153);
-  line(0,windowHeight/2,windowHeight,windowHeight/2)
-  line(0,windowHeight/3,windowHeight,windowHeight/3)
-  line(0,windowHeight/6,windowHeight,windowHeight/6)
-  line(0,windowHeight/6+(windowHeight/2),windowHeight,(windowHeight/6)+(windowHeight/2))
-  line(0,windowHeight/3+(windowHeight/2),windowHeight,(windowHeight/3)+(windowHeight/2))
-fill(0)
-strokeWeight(23)
 
-}
+var nota_attuale = freq[1];
 
-var nota_attuale=freq[1];
+window.addEventListener('load', function(){ // on page load
+
+    document.body.addEventListener('touchmove', function(e){
+      envAttack();
+
+      allParticles.push(new Particle(mouseX, mouseY, maxLevel));
+
+      console.log("move Y:" +e.changedTouches[0].pageY) // alert pageX coordinate of touch point
+
+
+
+
+    },
+    false)
+
+
+}, false)
+
+
+
+
+
 
 function mouseDragged() {
-    envAttack();
-  allParticles.push(new Particle(mouseX, mouseY, maxLevel));
+
+  // envAttack();
+  //
+  // allParticles.push(new Particle(mouseX, mouseY, maxLevel));
   // griglia()
-if (mouseY>0 && mouseY<(windowHeight/2)/2) {
-  console.log("Soglia 1")
+  if (mouseY > 0 && mouseY < (windowHeight / 2) / 2) {
+    // console.log("Soglia 1")
 
-  masterVolume(map(mouseX,0,windowWidth,0.1,1.));
-  // nota_attuale++
-    triOsc.freq( nota_attuale)
-}
-if (mouseY>(windowHeight/3) && mouseY<(windowHeight/2)){
-  console.log("Soglia 2")
- masterVolume(map(mouseX,0,windowWidth,0.1,1.));
 
-    triOsc.freq( freq[2])
-}
-if (mouseY>(windowHeight/2) && mouseY<windowHeight/6+(windowHeight/2)){
-  console.log("Soglia 3")
-   masterVolume(map(mouseX,0,windowWidth,0.1,1.));
 
-    triOsc.freq( freq[3])
+    // nota_attuale++
+    triOsc.freq(nota_attuale)
+  }
+  if (mouseY > (windowHeight / 3) && mouseY < (windowHeight / 2)) {
+    // console.log("Soglia 2")
 
-}
-if (mouseY>windowHeight/6+(windowHeight/2) && mouseY<(windowHeight+(windowHeight/2))/2){
-  console.log("Soglia 4")
- masterVolume(map(mouseX,0,windowWidth,0.1,1.));
 
-    triOsc.freq( freq[4])
+    triOsc.freq(freq[2])
+  }
+  if (mouseY > (windowHeight / 2) && mouseY < windowHeight / 6 + (windowHeight / 2)) {
+    // console.log("Soglia 3")
 
-}
-if (mouseY>(windowHeight+(windowHeight/2))/2 && mouseY<windowHeight/3+(windowHeight+2)){
-  console.log("Soglia 5")
- masterVolume(map(mouseX,0,windowWidth,0.1,1.));
 
-    triOsc.freq( freq[5])
+    triOsc.freq(freq[3])
 
-}
+  }
+  if (mouseY > windowHeight / 6 + (windowHeight / 2) && mouseY < (windowHeight + (windowHeight / 2)) / 2) {
+    // console.log("Soglia 4")
 
+
+    triOsc.freq(freq[4])
+
+  }
+  if (mouseY > (windowHeight + (windowHeight / 2)) / 2 && mouseY < windowHeight / 3 + (windowHeight + 2)) {
+    // console.log("Soglia 5")
+
+
+    triOsc.freq(freq[5])
+
+  }
+  // masterVolume(map(mouseX,0,windowWidth,0.1,0.6));
 }
